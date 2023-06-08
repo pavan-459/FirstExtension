584c0fe53f2bbfa98cb5509973521ce6d8678ea1
pavan-459 <pavan459@protonmail.ch>
2023-06-08 15:05:15 +0530

    your - my

--- popup.tsx
+++ popup.tsx
@@ -11,7 +11,7 @@ function IndexPopup() {
         padding: 16
       }}>
       <h2>
-        Welcome to your{" "}
+        Welcome to My{" "}
         <a href="https://www.plasmo.com" target="_blank">
           Plasmo
         </a>{" "}
